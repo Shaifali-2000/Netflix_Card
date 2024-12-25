5f5130b5dc72ad9671529d86488cc39e240ac1c8
shaifali <shaifalikatiyar428@gmail.com>
2024-12-25 13:54:33 +0530

    Destrucing data

--- src/assets/SeriesCard.jsx
+++ src/assets/SeriesCard.jsx
@@ -1,22 +1,28 @@
 import React from "react";
+
 const SeriesCard = (props) => {
+	const { data } = props;
+
+	// Ensure `data` is valid
+	if (!data) {
+		return <p>Data is not available.</p>;
+	}
+
 	return (
-		<li key={props.curlElem.id}>
-			<img
-				src={props.curlElem.img_url}
-				alt={props.curlElem.name}
-				width="40%"
-				height="40%"
-			/>
-			<h2>Name: {props.curlElem.name}</h2>
-			<h3>Rating:{props.curlElem.rating}</h3>
-			<p>Description: {props.curlElem.description}</p>
-			<p>Cast: {props.curlElem.cast}</p>
-			<p>Genre: {props.curlElem.genre}</p>
-			<a href={props.curlElem.watch_url}>
-				<button>Watch Now</button>
-			</a>
-		</li>
+		<ul>
+			<li>
+				<img src={data.img_url} alt={data.name} width="40%" height="40%" />
+				<h2>Name: {data.name}</h2>
+				<h3>Rating: {data.rating}</h3>
+				<p>Description: {data.description}</p>
+				<p>Cast: {data.cast}</p>
+				<p>Genre: {data.genre}</p>
+				<a href={data.watch_url}>
+					<button>Watch Now</button>
+				</a>
+			</li>
+		</ul>
 	);
 };
+
 export default SeriesCard;
